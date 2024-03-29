4bc1a3f80e7edc57655fa7d6d900e3a15a608629
Vladislav <144445267+TrofimovVladislav5@users.noreply.github.com>
2024-03-29 19:56:06 +0300

    Delete max.c

--- max.c
+++
@@ -1,9 +0,0 @@
-int max(int arr[],int count){
-    int max_index = 0;
-    for (int i = 1; i < count; i++) {
-        if (arr[i] > arr[max_index]) {
-            max_index = i;
-        }
-    }
-    return max_index;
-}
