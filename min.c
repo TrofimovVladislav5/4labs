a5b9b6b19a5e9cb08686fa78d79221667d5873f9
Vladislav <144445267+TrofimovVladislav5@users.noreply.github.com>
2024-03-29 19:56:22 +0300

    Delete min.c

--- min.c
+++
@@ -1,9 +0,0 @@
-int min(int arr[],int count){
-    int min_index = 0;
-    for(int i = 1; i < count; i++) {
-        if (arr[i] < arr[min_index]){
-            min_index = i;
-        }
-    }
-    return min_index;
-}
