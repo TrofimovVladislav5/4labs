68c70c2096b1960cbe9a120801e3ea9347bac73e
Vladislav <144445267+TrofimovVladislav5@users.noreply.github.com>
2024-09-27 00:35:31 +0300

    Delete WarShips directory

--- WarShips/GameState.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameState.h"
-void GameState::openState(){};
-
-void GameState::updateState(){};
-
-void GameState::closeState(){};
-
-bool GameState::transitToState(GameState* newState){};
--- WarShips/GameView.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-class GameView{
-public:
-    void displayMessage(const std::string& message);
-    void displayGameField();
-};
